9278f7d2f6c787e1dc3714780de0166d2287173d
newodev <92925014+newodev@users.noreply.github.com>
2021-12-20 22:52:32 +1030

    Implemented coyote time for jumping

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -18,9 +18,13 @@ public class PlayerMovement : NetworkBehaviour
 
     private bool canMoveCamera = true;
 
-    private float jumpCooldown;
+    private float jumpCooldown = 1.5f;
+    private float currentJumpCooldown;
 
     private bool onGround;
+
+    private float coyoteTime = 0.2f;
+    private float currentCoyoteTime;
 
     // Start is called before the first frame update
     void Start()
@@ -114,36 +118,68 @@ public class PlayerMovement : NetworkBehaviour
 
     private void UpdateGroundCheck()
     {
+        // How far below the hitbox the ground is measured
         float groundCheckDistance = 0.05f;
+        // Check for environment colliders in a small area below our hitbox
         Vector3 p = new Vector3(collider.bounds.center.x, collider.bounds.center.y - collider.bounds.extents.y - (groundCheckDistance / 2f), collider.bounds.center.z);
+        bool newOnGround = Physics.OverlapSphere(p, groundCheckDistance / 2f).Where(x => x.tag == "Environment").Count() > 0;
 
-        onGround = Physics.OverlapSphere(p, groundCheckDistance / 2f).Where(x => x.tag == "Environment").Count() > 0;
+        // If we were on the ground last frame, and not anymore, begin coyote time countdown
+        if (onGround && !newOnGround)
+        {
+            currentCoyoteTime = coyoteTime;
+        }
+        // Decrement coyote time until 0
+        if (currentCoyoteTime >= 0)
+        {
+            currentCoyoteTime -= Time.fixedDeltaTime;
+        }
+        // Update onGround
+        onGround = newOnGround;
     }
 
     private void UpdateJump()
     {
-        if (jumpCooldown > 0)
+        // Decrement jump cooldown
+        if (currentJumpCooldown > 0)
         {
-            jumpCooldown -= Time.fixedDeltaTime;
+            currentJumpCooldown -= Time.fixedDeltaTime;
         }
-        if (onGround && input.GetJumpKeyPressed() && jumpCooldown <= 0)
+        // Standard jump check
+        if (onGround && input.GetJumpKeyPressed() && currentJumpCooldown <= 0)
         {
             rb.AddForce(new Vector3(0f, stats.JumpForce, 0f), ForceMode.Impulse);
-            jumpCooldown = 1f;
+            currentJumpCooldown = jumpCooldown;
+        }
+        // Coyote jump check
+        else if (currentCoyoteTime > 0 && input.GetJumpKeyPressed() && currentJumpCooldown <= 0)
+        {
+            rb.velocity = new Vector3(0f, stats.JumpForce, 0f);
+            currentJumpCooldown = jumpCooldown;
+            currentCoyoteTime = 0f;
         }
     }
 
     [Command]
     private void CmdUpdateJump()
     {
-        if (jumpCooldown > 0)
+        // Decrement jump cooldown
+        if (currentJumpCooldown > 0)
         {
-            jumpCooldown -= Time.fixedDeltaTime;
+            currentJumpCooldown -= Time.fixedDeltaTime;
         }
-        if (onGround && input.GetJumpKeyPressed() && jumpCooldown <= 0)
+        // Standard jump check
+        if (onGround && input.GetJumpKeyPressed() && currentJumpCooldown <= 0)
         {
             rb.AddForce(new Vector3(0f, stats.JumpForce, 0f), ForceMode.Impulse);
-            jumpCooldown = 1f;
+            currentJumpCooldown = jumpCooldown;
+        }
+        // Coyote jump check
+        else if (currentCoyoteTime > 0 && input.GetJumpKeyPressed() && currentJumpCooldown <= 0)
+        {
+            rb.velocity = new Vector3(0f, stats.JumpForce, 0f);
+            currentJumpCooldown = jumpCooldown;
+            currentCoyoteTime = 0f;
         }
     }
 }
