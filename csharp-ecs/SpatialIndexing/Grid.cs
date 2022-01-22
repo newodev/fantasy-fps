db1ef569417a976687964e57bb7a66b518f7e743
newodev <92925014+newodev@users.noreply.github.com>
2022-01-23 00:01:34 +1030

    Beginning a very poor spatial indexing grid implementation

---
+++ csharp-ecs/SpatialIndexing/Grid.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_ECS.SpatialIndexing
+{
+    class Grid : ISpatialIndex
+    {
+        GridNode[,] nodes = new GridNode[30, 30];
+        int NodeWidth = 100;
+        public void Delete(int element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int element, Point p)
+        {
+            // TODO: fix/finish
+            Point node = FindNode(p);
+            nodes[(int)node.X, (int)node.Y].entities.Add(new EntityFootprint());
+        }
+
+        public List<int> Query(AABB box)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int element, Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Translates a point in world space to the node coordinate.
+        public Point FindNode(Point p)
+        {
+            return new Point((int)(p.X / NodeWidth), (int)(p.Y / NodeWidth));
+        }
+    }
+
+    internal struct GridNode
+    {
+        public List<EntityFootprint> entities;
+    }
+
+    struct EntityFootprint
+    {
+        public int ID;
+        public List<Type> Archetype;
+        public Point Position;
+    }
+}
