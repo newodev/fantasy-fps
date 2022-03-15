17d3837da9a638f0652bbf9c13621c099c3981d6
newodev <92925014+newodev@users.noreply.github.com>
2022-03-15 19:37:41 +1030

    Working on engine architecture

---
+++ Mechanics/Window.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Windowing.Desktop;
+
+namespace Game
+{
+    class Window : GameWindow
+    {
+
+        public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
+        {
+            
+        }
+    }
+}
