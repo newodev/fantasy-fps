c795b62300bd16e063d7973cf753843c795be0c0
newodev <92925014+newodev@users.noreply.github.com>
2022-05-04 21:31:34 +0930

    Some more small ecosim updates

--- Game/Ecosim/Item.cs
+++ Game/Ecosim/Item.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CSharp_ECS;
 
 namespace Game.Ecosim;
 
-// Systems used in the ecosim can sample state maps such as danger
-abstract class EcosimSystem : JobSystem
+class Item
 {
+    public string Name;
+    public string Description;
 
+    public float Value;
+    public string[] Tags;
 }
