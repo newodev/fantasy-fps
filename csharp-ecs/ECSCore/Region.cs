3fc6186bbc83c1eb8aeca06df3144febb4db3e8b
ax <92925014+newodev@users.noreply.github.com>
2022-05-12 12:20:20 +0930

    Solved namespace errors

--- csharp-ecs/ECSCore/Region.cs
+++ csharp-ecs/ECSCore/Region.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CSharp_ECS.ECSCore.Exceptions;
+using CSharp_ECS.Exceptions;
 using System.Reflection;
 
 namespace CSharp_ECS;
