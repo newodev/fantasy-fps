ca200a97b95042f7520f0f09ca5362df856c2fee
newodev <92925014+newodev@users.noreply.github.com>
2022-05-15 17:59:20 +0930

    Added depth buffer

--- Game/NewRendering/GBufferStage.cs
+++ Game/NewRendering/GBufferStage.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenTK.Graphics.OpenGL;
+using Game.Rendering;
 
 namespace Game.NewRendering;
 
@@ -11,6 +12,7 @@ class GBufferStage : RenderStage
 {
     private DrawBuffersEnum[] layers = { DrawBuffersEnum.ColorAttachment0, DrawBuffersEnum.ColorAttachment1, DrawBuffersEnum.ColorAttachment2, DrawBuffersEnum.ColorAttachment3 };
     private int buffer;
+    private Shader shader;
 
     public override void Init()
     {
@@ -23,11 +25,26 @@ class GBufferStage : RenderStage
         colorLayer = GenTarget(2, PixelInternalFormat.Rgba);
         detailLayer = GenTarget(3, PixelInternalFormat.Rgba);
 
-
         GL.DrawBuffers(4, layers);
+
+
+        int depthBuffer;
+        GL.GenRenderbuffers(1, out depthBuffer);
+        GL.BindRenderbuffer(RenderbufferTarget.Renderbuffer, depthBuffer);
+        GL.RenderbufferStorage(RenderbufferTarget.Renderbuffer, RenderbufferStorage.DepthComponent, Settings.Width, Settings.Height);
+        GL.FramebufferRenderbuffer(FramebufferTarget.Framebuffer, FramebufferAttachment.DepthAttachment, RenderbufferTarget.Renderbuffer, depthBuffer);
+
+        if (GL.CheckFramebufferStatus(FramebufferTarget.Framebuffer) != FramebufferErrorCode.FramebufferComplete)
+            Console.WriteLine("Buffer didn't complete");
+
+        GL.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
+
+        shader = new("Resources/Shaders/Standard/Opaque/shader.vert", "Resources/Shaders/Standard/Deferred/gbuffer.frag");
+        
     }
     public override void Render()
     {
+
 
     }
 }
---
+++ Game/NewRendering/LightingStage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL;
+using Game.Rendering;
+
+namespace Game.NewRendering;
+
+class LightingStage : RenderStage
+{
+    private Shader shader;
+    public override void Init()
+    {
+        shader = new("Resources/Shaders/Standard/Deferred/lighting.vert", "Resources/Shaders/Standard/Deferred/lighting.frag");
+        shader.SetInt("gPosition", 0);
+        shader.SetInt("gNormal", 1);
+        shader.SetInt("gAlbedo", 2);
+        shader.SetInt("gDetail", 3);
+    }
+    public override void Render()
+    {
+
+    }
+}
